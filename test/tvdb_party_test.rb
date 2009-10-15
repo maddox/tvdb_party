cd98472921934ba34ae81e85eb55d621941b6f3e
Jon Maddox <jon@jonsthoughtsoneverything.com>
2009-10-15 19:57:29 -0400

    tests

--- test/tvdb_party_test.rb
+++ test/tvdb_party_test.rb
@@ -2,16 +2,39 @@ require 'test_helper'
 
 class TvdbPartyTest < Test::Unit::TestCase
   context "tvdb search" do
+    setup do 
+      @tvdb = TvdbParty::Search.new('A97A9243F8030477')
+    end
   
     context "search for terrible query" do
       setup do
-        tvdb = TvdbParty::Search.new('A97A9243F8030477')
-        @results = tvdb.search("sdfsafsdfds")
+        @results = @tvdb.search("sdfsafsdfds")
       end
       
       should "have 0 results" do
-        puts @results.inspect
         assert_equal 0, @results.size
+      end
+
+    end
+
+    context "search for real show" do
+      setup do
+        @results = @tvdb.search("jojos")
+      end
+      
+      should "have 0 results" do
+        assert_equal 2, @results.size
+      end
+      
+      context "get the series" do
+        setup do 
+          @series = @tvdb.get_series_by_id(@results.first['seriesid'])
+        end
+        
+        should "have a series" do
+          assert_equal TvdbParty::Series, @series.class
+        end
+
       end
 
     end
