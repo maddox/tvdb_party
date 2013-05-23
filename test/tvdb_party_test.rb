b6bd736c24b7099238bd8bf7c55c27c8d6d7f9d5
Rafael Sachetto <rafael@sachetto.com.br>
2013-05-23 19:19:10 -0300

    testing if have seasons

--- test/tvdb_party_test.rb
+++ test/tvdb_party_test.rb
@@ -4,52 +4,56 @@ require 'test_helper'
 
 class TvdbPartyTest < Test::Unit::TestCase
   context "tvdb" do
-    setup do 
+    setup do
       @tvdb = TvdbParty::Search.new('A97A9243F8030477')
     end
-  
+
     context "search for terrible query" do
       setup do
         @results = @tvdb.search("sdfsafsdfds")
       end
-      
+
       should "have 0 results" do
         assert_equal 0, @results.size
       end
-    
+
     end
 
     context "search for episode that doesn't exist" do
       setup do
         @show = @tvdb.get_series_by_id(75700)
       end
-      
+
       should "have handle 404 on episode query" do
         assert_equal nil, @show.get_episode(1, 17)
       end
-    
+
     end
 
     context "search for real show" do
       setup do
         @results = @tvdb.search("The Office US")
       end
-      
+
       should "have 1 results" do
         assert_equal 1, @results.size
       end
-      
+
       context "get the series" do
-        setup do 
+        setup do
           @series = @tvdb.get_series_by_id(@results.first['seriesid'])
         end
-        
+
         should "have a series" do
           assert_equal TvdbParty::Series, @series.class
         end
 
         should "have actors" do
           assert_not_equal 0, @series.actors.size
+        end
+
+        should "have seasons" do
+          assert_not_equal 0, @series.seasons.length
         end
 
         should "have actual Actors" do
@@ -64,9 +68,9 @@ class TvdbPartyTest < Test::Unit::TestCase
           assert_equal 'tt0386676', @series.imdb_id
         end
       end
-    
+
     end
-  
+
   end
   context "non english series" do
     setup do
