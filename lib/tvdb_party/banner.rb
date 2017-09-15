a1689f48c17db82d68c33b74fa233755d4649e62
Jon Maddox <jon@jonmaddox.com>
2017-09-15 16:23:27 -0400

    use https

--- lib/tvdb_party/banner.rb
+++ lib/tvdb_party/banner.rb
@@ -1,7 +1,7 @@
 module TvdbParty
   class Banner
     attr_accessor :banner_type, :banner_type2, :season, :path, :thumbnail_path, :language
-    
+
     def initialize(options={})
       @banner_type = options["BannerType"]
       @banner_type2 = options["BannerType2"]
@@ -9,13 +9,13 @@ module TvdbParty
       @path = options["BannerPath"]
       @language = options["Language"]
     end
-    
+
     def url
-      "http://thetvdb.com/banners/" + @path
+      "https://thetvdb.com/banners/" + @path
     end
 
     def thumb_url
-      "http://thetvdb.com/banners/_cache/" + @path
+      "https://thetvdb.com/banners/_cache/" + @path
     end
 
   end
